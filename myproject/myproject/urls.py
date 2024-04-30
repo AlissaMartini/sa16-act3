d0844c48d084f79507c6a050a86349808a1c2e70
Alissa Martini <mmrtini4@memphis.edu>
2024-04-29 21:25:18 -0500

    updated with nav bar and fixed image with no image tag

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic.base import RedirectView
 from pages import views
 
-
 urlpatterns = [
-  path('admin/', admin.site.urls),
-  path('home/', views.home, name='home'),
-  path('about/', views.about, name='about'),
-  path('work/', views.work, name='work'),
-  path('contact/', views.contact, name='contact'),
+    path('admin/', admin.site.urls),
+    path('index/', RedirectView.as_view(url='/home/')),  # Redirect from '/' to '/home/'
+    path('home/', views.home),
+    path('about/', views.about),
+    path('contact/', views.contact),
+    path('work/', views.work),
 ]
--- myproject/pages/views.py
+++ myproject/pages/views.py
@@ -7,7 +7,14 @@ def about(request):
     return render(request, 'pages/about.html')
 
 def work(request):
-    return render(request, 'pages/work.html')
+    context = {
+        'image1_url': '/path/to/Ai.jpg',
+        'image2_url': '/path/to/youtube.jpg',
+        'image3_url': '/path/to/fortnite.jpg',
+    }
+    return render(request, 'pages/work.html',context)
 
 def contact(request):
     return render(request, 'pages/contact.html')
+from django.shortcuts import render
+
